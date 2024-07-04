33cf8eace36013da7458d055d416ff7a0ecd874e
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-07-04 05:52:03 -0300

    adjustment scroll disable in body

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AngularTechsLogosModule } from 'angular-techs-logos';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
@@ -21,15 +21,19 @@ import { AppFooterComponent } from './components/app-footer/app-footer.component
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  constructor(private renderer: Renderer2) {}
+
   codeString: string = '<angular-techs-logos />';
 
   isModalActive: boolean = false;
 
   openModal(): void {
     this.isModalActive = true;
+    this.renderer.addClass(document.body, 'modal-active');
   }
 
   closeModal(): void {
     this.isModalActive = false;
+    this.renderer.removeClass(document.body, 'modal-active');
   }
 }
