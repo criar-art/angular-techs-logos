014c107766d22cb74092f827948b00a37d54bd1e
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-07-04 08:53:36 -0300

    test(unit): improvement spec Header

--- src/app/components/app-header/app-header.component.spec.ts
+++ src/app/components/app-header/app-header.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { AppHeaderComponent } from './app-header.component';
 
 describe('AppHeaderComponent', () => {
@@ -8,9 +8,8 @@ describe('AppHeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppHeaderComponent]
-    })
-    .compileComponents();
+      imports: [AppHeaderComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AppHeaderComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,20 @@ describe('AppHeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the logo link with correct attributes', () => {
+    const logoLink = fixture.debugElement.query(By.css('.logo')).nativeElement;
+    expect(logoLink.href).toBe('https://www.npmjs.com/package/angular-techs-logos');
+    expect(logoLink.getAttribute('target')).toBe('_blank');
+  });
+
+  it('should render the Angular logo SVG with correct attributes', () => {
+    const svgElement = fixture.debugElement.query(By.css('.angular-logo')).nativeElement;
+    expect(svgElement.getAttribute('width')).toBe('80');
+    expect(svgElement.getAttribute('xmlns')).toBe('http://www.w3.org/2000/svg');
+    expect(svgElement.getAttribute('viewBox')).toBe('0 0 232 239');
+    expect(svgElement.getAttribute('fill')).toBe('none');
+    expect(svgElement.getAttribute('class')).toContain('angular-logo');
   });
 });
