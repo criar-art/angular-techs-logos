d71dcdecce605c69cd15e59ccb0b66b918768cef
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-07-04 08:00:40 -0300

    export techs-data of lib

--- projects/angular-techs-logos/src/public-api.ts
+++ projects/angular-techs-logos/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/angular-techs-logos.service';
 export * from './lib/angular-techs-logos.component';
 export * from './lib/angular-techs-logos.module';
+export * from './lib/techs/techs-data';
