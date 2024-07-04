a26253a7ed196fc470b38bbe6d3e7facf7884b76
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-07-04 05:07:54 -0300

    add footer with version angular and npm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { AngularTechsLogosModule } from 'angular-techs-logos';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
 import { DarkmodeComponent } from './components/darkmode/darkmode.component';
+import { AppFooterComponent } from './components/app-footer/app-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AngularTechsLogosModule, AppHeaderComponent, DarkmodeComponent],
+  imports: [
+    RouterOutlet,
+    AngularTechsLogosModule,
+    AppHeaderComponent,
+    DarkmodeComponent,
+    AppFooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
