04e5286794f0b1d56f2887e190fbf50cc2868625
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-07-04 04:17:20 -0300

    improvement styles of spa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AngularTechsLogosComponent } from 'angular-techs-logos';
+import { AngularTechsLogosModule } from 'angular-techs-logos';
+import { AppHeaderComponent } from './components/app-header/app-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AngularTechsLogosComponent],
+  imports: [RouterOutlet, AngularTechsLogosModule, AppHeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-techs-logos';
