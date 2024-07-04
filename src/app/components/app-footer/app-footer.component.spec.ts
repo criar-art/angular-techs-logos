c84112093e01df85b9eb1cc0f51883c8f24dc3ea
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-07-04 08:48:22 -0300

    test(unit): improvement spec Footer

--- src/app/components/app-footer/app-footer.component.spec.ts
+++ src/app/components/app-footer/app-footer.component.spec.ts
@@ -21,4 +21,29 @@ describe('AppFooterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the correct footer structure', () => {
+    const footerElement: HTMLElement = fixture.nativeElement;
+    expect(footerElement.querySelector('.footer')).toBeTruthy();
+
+    const linkElement: HTMLAnchorElement | null = footerElement.querySelector('a');
+    expect(linkElement).toBeTruthy();
+    if (linkElement) {
+      expect(linkElement.getAttribute('href')).toBe('https://github.com/criar-art/angular-techs-logos');
+      expect(linkElement.getAttribute('target')).toBe('_blank');
+    }
+
+    const appVersionComponent = footerElement.querySelector('app-version');
+    expect(appVersionComponent).toBeTruthy();
+
+    const svgElement: SVGSVGElement | null = footerElement.querySelector('svg');
+    expect(svgElement).toBeTruthy();
+    if (svgElement) {
+      expect(svgElement.getAttribute('viewBox')).toBe('0 0 25 25');
+      expect(svgElement.getAttribute('width')).toBe('30');
+      expect(svgElement.getAttribute('xmlns')).toBe('http://www.w3.org/2000/svg');
+    }
+
+    expect(footerElement.textContent).toContain('@lucasferreiralimax');
+  });
 });
