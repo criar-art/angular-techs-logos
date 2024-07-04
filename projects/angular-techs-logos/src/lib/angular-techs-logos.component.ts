d3a7c11143e40dbb2480f2851d330b62fa5bf9cb
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-07-04 07:59:04 -0300

    config search app and angular-techs-logos with signals

--- projects/angular-techs-logos/src/lib/angular-techs-logos.component.ts
+++ projects/angular-techs-logos/src/lib/angular-techs-logos.component.ts
@@ -8,7 +8,7 @@ import {
   ViewContainerRef,
   Type,
   ViewEncapsulation,
-  ChangeDetectorRef
+  signal
 } from '@angular/core';
 
 import { techs } from './techs/techs-data';
@@ -33,55 +33,74 @@ export class AngularTechsLogosComponent implements OnInit, OnChanges {
   @Input() class: string | undefined;
   @Input() hiddenLogos: string[] | undefined;
 
-  @ViewChild('iconContainer', { read: ViewContainerRef }) iconContainer!: ViewContainerRef;
+  @ViewChild('iconContainer', { read: ViewContainerRef })
+  iconContainer!: ViewContainerRef;
 
   techs: Tech[] = techs;
-
-  constructor(
-    private cdr: ChangeDetectorRef
-  ) {}
+  listSignal = signal<string[] | undefined>([]);
+  hiddenLogosSignal = signal<string[] | undefined>([]);
+  techsList = signal<Tech[]>(this.techs);
 
   ngOnInit(): void {
-    if(!this.name) {
+    if (!this.name) {
       this.updateTechs();
     }
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['list'] || changes['hiddenLogos']) {
+    if (changes['list']) {
+      this.listSignal.set(this.list);
+    }
+    if (changes['hiddenLogos']) {
+      this.hiddenLogosSignal.set(this.hiddenLogos);
+    }
+
+    if (!this.name) {
       this.updateTechs();
     }
   }
 
   private updateTechs(): void {
-    if (this.list) {
-      this.techs = this.getTechs(this.list);
-    } else if (this.hiddenLogos) {
-      this.techs = this.hiddenTechs(this.hiddenLogos);
+    const currentList = this.listSignal();
+    const currentHiddenLogos = this.hiddenLogosSignal();
+
+    if (currentList) {
+      this.techsList.set(this.getTechs(currentList));
+    } else if (currentHiddenLogos && currentHiddenLogos.length > 0) {
+      this.techsList.set(this.hiddenTechs(currentHiddenLogos));
     } else {
-      this.techs = this.techs;
+      this.techsList.set(this.techs);
     }
   }
 
-  public techClass(name: string) {
+  public techClass(name: string): string[] {
     const className = name?.toLowerCase() || '';
     return [this.class || '', className];
   }
 
   public getTech(name: string): Tech | undefined {
-    return this.techs.find(item => item.name.toLowerCase() === name.toLowerCase());
+    return this.techs.find(
+      (item) => item.name.toLowerCase() === name.toLowerCase()
+    );
   }
 
   private getTechs(items: string[]): Tech[] {
-    return this.techs.filter(tech => items.some(item => tech.name.toLowerCase() === item.toLowerCase()));
+    return this.techs.filter((tech) => {
+      if (typeof tech.name !== 'string') return false;
+      return items.some((item) => {
+        if (typeof item !== 'string') return false;
+        return tech.name.toLowerCase() === item.toLowerCase();
+      });
+    });
   }
 
   private hiddenTechs(items: string[]): Tech[] {
-    return this.techs.filter(tech => !items.includes(tech.name.toLowerCase()));
+    return this.techs.filter(
+      (tech) => !items.includes(tech.name.toLowerCase())
+    );
   }
 
   public getIcon(name: string): Type<any> | null {
-    console.log("getIcon name icon: ", name)
     const tech = this.getTech(name);
     return tech ? tech.icon : null;
   }
