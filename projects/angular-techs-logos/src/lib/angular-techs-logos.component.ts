af73250f9504ce7eed9e9fa3be068340eb4d1728
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-07-04 04:16:44 -0300

    config template and logic in angular-techs-logos

--- projects/angular-techs-logos/src/lib/angular-techs-logos.component.ts
+++ projects/angular-techs-logos/src/lib/angular-techs-logos.component.ts
@@ -1,14 +1,90 @@
-import { Component } from '@angular/core';
-// import { SvgTestComponent } from '@app/techs/amd'
-import { SvgTestComponent } from './techs/svg-test/svg-test.component'
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+  ViewContainerRef,
+  Type,
+  ViewEncapsulation,
+  ChangeDetectorRef
+} from '@angular/core';
+
+import { techs } from './techs/techs-data';
+
+interface Tech {
+  name: string;
+  icon: Type<any>;
+}
 
 @Component({
   selector: 'lib-angular-techs-logos',
-  standalone: true,
-  imports: [SvgTestComponent],
   templateUrl: './angular-techs-logos.component.html',
-  styleUrl: './angular-techs-logos.component.scss'
+  styleUrls: ['./angular-techs-logos.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AngularTechsLogosComponent {
+export class AngularTechsLogosComponent implements OnInit, OnChanges {
+  @Input() list: string[] | undefined;
+  @Input() name: string | undefined;
+  @Input() size: string | undefined;
+  @Input() label: string | undefined;
+  @Input() hiddenLabel: boolean | undefined;
+  @Input() class: string | undefined;
+  @Input() hiddenLogos: string[] | undefined;
 
+  @ViewChild('iconContainer', { read: ViewContainerRef }) iconContainer!: ViewContainerRef;
+
+  techs: Tech[] = techs;
+
+  constructor(
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    if(!this.name) {
+      this.updateTechs();
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['list'] || changes['hiddenLogos']) {
+      this.updateTechs();
+    }
+  }
+
+  private updateTechs(): void {
+    if (this.list) {
+      this.techs = this.getTechs(this.list);
+    } else if (this.hiddenLogos) {
+      this.techs = this.hiddenTechs(this.hiddenLogos);
+    } else {
+      this.techs = this.techs;
+    }
+
+    console.log('this.techs: ', this.techs)
+  }
+
+  public techClass(name: string) {
+    const className = name?.toLowerCase() || '';
+    return [this.class || '', className];
+  }
+
+  public getTech(name: string): Tech | undefined {
+    return this.techs.find(item => item.name.toLowerCase() === name.toLowerCase());
+  }
+
+  private getTechs(items: string[]): Tech[] {
+    return this.techs.filter(tech => items.some(item => tech.name.toLowerCase() === item.toLowerCase()));
+  }
+
+  private hiddenTechs(items: string[]): Tech[] {
+    return this.techs.filter(tech => !items.includes(tech.name.toLowerCase()));
+  }
+
+  public getIcon(name: string): Type<any> | null {
+    console.log("getIcon name icon: ", name)
+    const tech = this.getTech(name);
+    return tech ? tech.icon : null;
+  }
 }
--- projects/angular-techs-logos/src/lib/angular-techs-logos.module.ts
+++ projects/angular-techs-logos/src/lib/angular-techs-logos.module.ts
@@ -1,11 +1,293 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AngularTechsLogosComponent } from './angular-techs-logos.component';
+import { FortranComponent } from './techs/fortran/fortran.component';
+import { SteamComponent } from './techs/steam/steam.component';
+import { GeminiComponent } from './techs/gemini/gemini.component';
+import { TumblrComponent } from './techs/tumblr/tumblr.component';
+import { CypressComponent } from './techs/cypress/cypress.component';
+import { DropboxComponent } from './techs/dropbox/dropbox.component';
+import { TurboComponent } from './techs/turbo/turbo.component';
+import { MicrosoftComponent } from './techs/microsoft/microsoft.component';
+import { ApacheComponent } from './techs/apache/apache.component';
+import { WebpackComponent } from './techs/webpack/webpack.component';
+import { JavascriptComponent } from './techs/javascript/javascript.component';
+import { LinuxComponent } from './techs/linux/linux.component';
+import { DrupalComponent } from './techs/drupal/drupal.component';
+import { AndroidComponent } from './techs/android/android.component';
+import { VueComponent } from './techs/vue/vue.component';
+import { MintComponent } from './techs/mint/mint.component';
+import { VisualcodeComponent } from './techs/visualcode/visualcode.component';
+import { VitestComponent } from './techs/vitest/vitest.component';
+import { GitlabComponent } from './techs/gitlab/gitlab.component';
+import { AwsComponent } from './techs/aws/aws.component';
+import { SassComponent } from './techs/sass/sass.component';
+import { RustComponent } from './techs/rust/rust.component';
+import { SupabaseComponent } from './techs/supabase/supabase.component';
+import { FirebaseComponent } from './techs/firebase/firebase.component';
+import { JavaComponent } from './techs/java/java.component';
+import { BitbucketComponent } from './techs/bitbucket/bitbucket.component';
+import { GithubComponent } from './techs/github/github.component';
+import { IbmComponent } from './techs/ibm/ibm.component';
+import { OracleComponent } from './techs/oracle/oracle.component';
+import { ManjaroComponent } from './techs/manjaro/manjaro.component';
+import { ExpoComponent } from './techs/expo/expo.component';
+import { CopilotComponent } from './techs/copilot/copilot.component';
+import { NpmComponent } from './techs/npm/npm.component';
+import { NuxtComponent } from './techs/nuxt/nuxt.component';
+import { CppComponent } from './techs/cpp/cpp.component';
+import { AngularComponent } from './techs/angular/angular.component';
+import { XComponent } from './techs/x/x.component';
+import { VkComponent } from './techs/vk/vk.component';
+import { PandasComponent } from './techs/pandas/pandas.component';
+import { MozillaComponent } from './techs/mozilla/mozilla.component';
+import { TailwindComponent } from './techs/tailwind/tailwind.component';
+import { InstagramComponent } from './techs/instagram/instagram.component';
+import { ShopifyComponent } from './techs/shopify/shopify.component';
+import { RubyComponent } from './techs/ruby/ruby.component';
+import { EdgeComponent } from './techs/edge/edge.component';
+import { SafariComponent } from './techs/safari/safari.component';
+import { KarmaComponent } from './techs/karma/karma.component';
+import { BackboneComponent } from './techs/backbone/backbone.component';
+import { PythonComponent } from './techs/python/python.component';
+import { PrimevideoComponent } from './techs/primevideo/primevideo.component';
+import { DockerComponent } from './techs/docker/docker.component';
+import { GitComponent } from './techs/git/git.component';
+import { OpensuseComponent } from './techs/opensuse/opensuse.component';
+import { NvidiaComponent } from './techs/nvidia/nvidia.component';
+import { XcodeComponent } from './techs/xcode/xcode.component';
+import { TelegramComponent } from './techs/telegram/telegram.component';
+import { TensorflowComponent } from './techs/tensorflow/tensorflow.component';
+import { RaspberryComponent } from './techs/raspberry/raspberry.component';
+import { ViteComponent } from './techs/vite/vite.component';
+import { ArchlinuxComponent } from './techs/archlinux/archlinux.component';
+import { LuaComponent } from './techs/lua/lua.component';
+import { CiscoComponent } from './techs/cisco/cisco.component';
+import { NextjsComponent } from './techs/nextjs/nextjs.component';
+import { SpotifyComponent } from './techs/spotify/spotify.component';
+import { KotlinComponent } from './techs/kotlin/kotlin.component';
+import { KubernetsComponent } from './techs/kubernets/kubernets.component';
+import { DeepinComponent } from './techs/deepin/deepin.component';
+import { MariadbComponent } from './techs/mariadb/mariadb.component';
+import { CentosComponent } from './techs/centos/centos.component';
+import { MongodbComponent } from './techs/mongodb/mongodb.component';
+import { SvelteComponent } from './techs/svelte/svelte.component';
+import { EclipseComponent } from './techs/eclipse/eclipse.component';
+import { UbuntuComponent } from './techs/ubuntu/ubuntu.component';
+import { DeezerComponent } from './techs/deezer/deezer.component';
+import { SnapdragonComponent } from './techs/snapdragon/snapdragon.component';
+import { FacebookComponent } from './techs/facebook/facebook.component';
+import { KalilinuxComponent } from './techs/kalilinux/kalilinux.component';
+import { PinterestComponent } from './techs/pinterest/pinterest.component';
+import { CssComponent } from './techs/css/css.component';
+import { ChatgptComponent } from './techs/chatgpt/chatgpt.component';
+import { JestComponent } from './techs/jest/jest.component';
+import { SpacexComponent } from './techs/spacex/spacex.component';
+import { YandexComponent } from './techs/yandex/yandex.component';
+import { StylusComponent } from './techs/stylus/stylus.component';
+import { WhatsappComponent } from './techs/whatsapp/whatsapp.component';
+import { GoogleComponent } from './techs/google/google.component';
+import { PhpComponent } from './techs/php/php.component';
+import { TypescriptComponent } from './techs/typescript/typescript.component';
+import { StackoverflowComponent } from './techs/stackoverflow/stackoverflow.component';
+import { VercelComponent } from './techs/vercel/vercel.component';
+import { FedoraComponent } from './techs/fedora/fedora.component';
+import { QwikComponent } from './techs/qwik/qwik.component';
+import { RedhatComponent } from './techs/redhat/redhat.component';
+import { PlaystoreComponent } from './techs/playstore/playstore.component';
+import { AndroidstudioComponent } from './techs/androidstudio/androidstudio.component';
+import { SlackwareComponent } from './techs/slackware/slackware.component';
+import { YoutubeComponent } from './techs/youtube/youtube.component';
+import { HpComponent } from './techs/hp/hp.component';
+import { GulpComponent } from './techs/gulp/gulp.component';
+import { AstroComponent } from './techs/astro/astro.component';
+import { PwaComponent } from './techs/pwa/pwa.component';
+import { EvernoteComponent } from './techs/evernote/evernote.component';
+import { CodepenComponent } from './techs/codepen/codepen.component';
+import { DribbbleComponent } from './techs/dribbble/dribbble.component';
+import { AmdComponent } from './techs/amd/amd.component';
+import { RedisComponent } from './techs/redis/redis.component';
+import { LinkedinComponent } from './techs/linkedin/linkedin.component';
+import { ReactComponent } from './techs/react/react.component';
+import { LessComponent } from './techs/less/less.component';
+import { SwiftComponent } from './techs/swift/swift.component';
+import { DiscordComponent } from './techs/discord/discord.component';
+import { OperaComponent } from './techs/opera/opera.component';
+import { JetbrainsComponent } from './techs/jetbrains/jetbrains.component';
+import { MacosComponent } from './techs/macos/macos.component';
+import { DebianComponent } from './techs/debian/debian.component';
+import { DuckduckgoComponent } from './techs/duckduckgo/duckduckgo.component';
+import { HtmlComponent } from './techs/html/html.component';
+import { SoundcloudComponent } from './techs/soundcloud/soundcloud.component';
+import { ElementaryosComponent } from './techs/elementaryos/elementaryos.component';
+import { TorrentComponent } from './techs/torrent/torrent.component';
+import { IntellijComponent } from './techs/intellij/intellij.component';
+import { EmberComponent } from './techs/ember/ember.component';
+import { MysqlComponent } from './techs/mysql/mysql.component';
+import { BaiduComponent } from './techs/baidu/baidu.component';
+import { WordpressComponent } from './techs/wordpress/wordpress.component';
+import { CsharpComponent } from './techs/csharp/csharp.component';
+import { SpringbootComponent } from './techs/springboot/springboot.component';
+import { TomcatComponent } from './techs/tomcat/tomcat.component';
+import { TorComponent } from './techs/tor/tor.component';
+import { GoComponent } from './techs/go/go.component';
+import { ChromeComponent } from './techs/chrome/chrome.component';
+import { AppleComponent } from './techs/apple/apple.component';
+import { NetflixComponent } from './techs/netflix/netflix.component';
+import { DigitaloceanComponent } from './techs/digitalocean/digitalocean.component';
+import { PostgresqlComponent } from './techs/postgresql/postgresql.component';
+import { NodeComponent } from './techs/node/node.component';
+import { TiktokComponent } from './techs/tiktok/tiktok.component';
+import { CobolComponent } from './techs/cobol/cobol.component';
+import { FirefoxComponent } from './techs/firefox/firefox.component';
+import { MaxComponent } from './techs/max/max.component';
+
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-  ],
-  exports: []
+  imports: [CommonModule],
+  exports: [AngularTechsLogosComponent],
+  declarations: [
+    AngularTechsLogosComponent,
+    FortranComponent,
+    SteamComponent,
+    GeminiComponent,
+    TumblrComponent,
+    CypressComponent,
+    DropboxComponent,
+    TurboComponent,
+    MicrosoftComponent,
+    ApacheComponent,
+    WebpackComponent,
+    JavascriptComponent,
+    LinuxComponent,
+    DrupalComponent,
+    AndroidComponent,
+    VueComponent,
+    MintComponent,
+    VisualcodeComponent,
+    VitestComponent,
+    GitlabComponent,
+    AwsComponent,
+    SassComponent,
+    RustComponent,
+    SupabaseComponent,
+    FirebaseComponent,
+    JavaComponent,
+    BitbucketComponent,
+    GithubComponent,
+    IbmComponent,
+    OracleComponent,
+    ManjaroComponent,
+    ExpoComponent,
+    CopilotComponent,
+    NpmComponent,
+    NuxtComponent,
+    CppComponent,
+    AngularComponent,
+    XComponent,
+    VkComponent,
+    PandasComponent,
+    MozillaComponent,
+    TailwindComponent,
+    InstagramComponent,
+    ShopifyComponent,
+    RubyComponent,
+    EdgeComponent,
+    SafariComponent,
+    KarmaComponent,
+    BackboneComponent,
+    PythonComponent,
+    PrimevideoComponent,
+    DockerComponent,
+    GitComponent,
+    OpensuseComponent,
+    NvidiaComponent,
+    XcodeComponent,
+    TelegramComponent,
+    TensorflowComponent,
+    RaspberryComponent,
+    ViteComponent,
+    ArchlinuxComponent,
+    LuaComponent,
+    CiscoComponent,
+    NextjsComponent,
+    SpotifyComponent,
+    KotlinComponent,
+    KubernetsComponent,
+    DeepinComponent,
+    MariadbComponent,
+    CentosComponent,
+    MongodbComponent,
+    SvelteComponent,
+    EclipseComponent,
+    UbuntuComponent,
+    DeezerComponent,
+    SnapdragonComponent,
+    FacebookComponent,
+    KalilinuxComponent,
+    PinterestComponent,
+    CssComponent,
+    ChatgptComponent,
+    JestComponent,
+    SpacexComponent,
+    YandexComponent,
+    StylusComponent,
+    WhatsappComponent,
+    GoogleComponent,
+    PhpComponent,
+    TypescriptComponent,
+    StackoverflowComponent,
+    VercelComponent,
+    FedoraComponent,
+    QwikComponent,
+    RedhatComponent,
+    PlaystoreComponent,
+    AndroidstudioComponent,
+    SlackwareComponent,
+    YoutubeComponent,
+    HpComponent,
+    GulpComponent,
+    AstroComponent,
+    PwaComponent,
+    EvernoteComponent,
+    CodepenComponent,
+    DribbbleComponent,
+    AmdComponent,
+    RedisComponent,
+    LinkedinComponent,
+    ReactComponent,
+    LessComponent,
+    SwiftComponent,
+    DiscordComponent,
+    OperaComponent,
+    JetbrainsComponent,
+    MacosComponent,
+    DebianComponent,
+    DuckduckgoComponent,
+    HtmlComponent,
+    SoundcloudComponent,
+    ElementaryosComponent,
+    TorrentComponent,
+    IntellijComponent,
+    EmberComponent,
+    MysqlComponent,
+    BaiduComponent,
+    WordpressComponent,
+    CsharpComponent,
+    SpringbootComponent,
+    TomcatComponent,
+    TorComponent,
+    GoComponent,
+    ChromeComponent,
+    AppleComponent,
+    NetflixComponent,
+    DigitaloceanComponent,
+    PostgresqlComponent,
+    NodeComponent,
+    TiktokComponent,
+    CobolComponent,
+    FirefoxComponent,
+    MaxComponent,
+  ]
 })
-export class AngularTechsLogosModule { }
+export class AngularTechsLogosModule {}
