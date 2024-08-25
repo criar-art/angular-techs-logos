63e78ba5f8549f0022c2ded0acb36fb5281aaa4a
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-08-25 06:04:18 -0300

    remove service

--- projects/angular-techs-logos/src/public-api.ts
+++ projects/angular-techs-logos/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of angular-techs-logos
  */
 
-export * from './lib/angular-techs-logos.service';
 export * from './lib/angular-techs-logos.component';
 export * from './lib/angular-techs-logos.module';
 export * from './lib/techs/techs-data';
